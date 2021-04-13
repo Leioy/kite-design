2c6bef5d7a9907a96e76a845934414b404245f7a
Leioy <13902303081@139.com>
2021-04-14 01:36:33 +0800

    fix: button types export

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,4 +1,4 @@
 import Button from './button';
 
-export { NativeButtonProps, ButtonShape, ButtonVariant, ButtonColor, ButtonHtmlType } from './button';
+export type { NativeButtonProps, ButtonShape, ButtonVariant, ButtonColor, ButtonHtmlType } from './button';
 export default Button;
